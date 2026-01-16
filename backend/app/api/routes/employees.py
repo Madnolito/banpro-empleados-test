a50c74854658a3264d92708473bd62686aea1459
Emanuel Tapia <emanuel.tapia.ext@telefonica.com>
2026-01-16 15:12:58 -0300

    :star: feat(api): add employees endpoints GET + UPDATE + DEACTIVATE - FastApi V0.0.3

--- backend/app/api/routes/employees.py
+++ backend/app/api/routes/employees.py
@@ -4,12 +4,15 @@ from fastapi import APIRouter, Depends, HTTPException, Query # (Depends) inyecci
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
-from app.schemas.employee import EmployeeCreate, EmployeeOut
+from app.schemas.employee import EmployeeCreate, EmployeeOut, EmployeeUpdate
 from app.crud.employee import (
     create_employee,
     get_employee_by_rut,
     list_employees,
-)
+    get_employee,
+    update_employee,
+    deactivate_employee,
+)	
 
 router = APIRouter(prefix="/employees", tags=["employees"])
 
@@ -47,3 +50,39 @@ def post_employee(payload: EmployeeCreate, db: Session = Depends(get_db)): # val
 
     return create_employee(db, payload) # db y body
 
+
+# ----------- PUT employee ----------- 
+
+@router.put("/{employee_id}", response_model=EmployeeOut)
+def put_employee(employee_id: int, payload: EmployeeUpdate, db: Session = Depends(get_db)):
+    employee = get_employee(db, employee_id)
+    if not employee:
+        raise HTTPException(status_code=404, detail="Empleado no encontrado!")
+
+    # si es que manda rut, se valida que no tope con otro
+    if payload.rut and payload.rut != employee.rut:
+        if get_employee_by_rut(db, payload.rut):
+            raise HTTPException(status_code=400, detail="Este RUT ya existe!")
+
+    return update_employee(db, employee, payload)
+
+
+# ----------- GET details -----------
+
+@router.get("/{employee_id}", response_model=EmployeeOut)
+def get_employee_detail(employee_id: int, db: Session = Depends(get_db)):
+    employee = get_employee(db, employee_id)
+    if not employee:
+        raise HTTPException(status_code=404, detail="Empleado no encontrado")
+    return employee
+
+
+# ----------- DELETE -----------
+
+@router.delete("/{employee_id}", response_model=EmployeeOut)
+def delete_employee(employee_id: int, db: Session = Depends(get_db)):
+    employee = get_employee(db, employee_id)
+    if not employee:
+        raise HTTPException(status_code=404, detail="Empleado no encontrado")
+
+    return deactivate_employee(db, employee)
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from app.core.config import settings
 from app.db.init_db import init_db
 from app.api.routes.employees import router as employees_router
 
-app = FastAPI(title="BANPRO Employees API", version="0.0.2",)
+app = FastAPI(title="BANPRO Employees API", version="0.0.3",)
 
 @app.on_event("startup")
 def on_startup():
