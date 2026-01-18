d27910ee03f715f99f7b47cbcd7cd5a73a25d761
Emanuel Tapia <emanuel.tapia.ext@telefonica.com>
2026-01-17 23:57:17 -0300

    :construction: feat(frontend): add initial React + Vite project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>BANPRO React App</h1>
+      <div className="card">
+        <p>
+          By Emanuel Tapia
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
